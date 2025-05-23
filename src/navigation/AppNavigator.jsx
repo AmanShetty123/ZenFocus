c0c70e5ca68216ec9a84d9a8200aef74ab6ae97b
Aman Shetty <shettyaman016@gmail.com>
2025-05-23 17:44:55 +0530

    signup screen created

--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
 import BottomTabNavigator from './BottomTabNavigator';
+import SignupScreen from '../screens/SignupScreen';
 
 const Stack = createStackNavigator();
 const AppNavigator = () => {
@@ -13,6 +14,7 @@ const AppNavigator = () => {
         <Stack.Navigator initialRouteName='login'>
             <Stack.Screen name='login' component={LoginScreen} options={{headerShown: false}}/>
             <Stack.Screen name='HomeTabs' component={BottomTabNavigator} options={{headerShown: false}}/>
+            <Stack.Screen name='signup' component={SignupScreen} options={{headerShown: false}} />
         </Stack.Navigator>
     </NavigationContainer>
   )
